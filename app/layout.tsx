ab6374613ec99aa7a1e5f659ba263c3f5339271d
Asep Angga Ihza S <126168431+anggaihza@users.noreply.github.com>
2024-11-30 13:14:33 +0700

    fix grid layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
+
 import "./globals.css";
 import {ThemeProvider} from "./provider";
 
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Angga's Portfolio",
-  description: "Modern & Minimalist JS Mastery Portfolio",
+  title: "Adrian's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
 };
 
 export default function RootLayout({
@@ -16,9 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
       <body className={inter.className}>
-        {" "}
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
