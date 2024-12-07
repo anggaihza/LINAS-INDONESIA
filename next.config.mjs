1ac5a26985cb82bc3224b81b6bc798e3c3f78ffc
Asep Angga Ihza S <126168431+anggaihza@users.noreply.github.com>
2024-12-07 08:09:13 +0700

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
