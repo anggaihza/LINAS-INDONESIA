c2f7db2cb3cf627351ee88f31ef7cba684005def
Asep Angga Ihza S <126168431+anggaihza@users.noreply.github.com>
2024-12-16 07:51:46 +0700

    added overflowed on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,9 @@ import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full overflow-clip pt-20 pb-10" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+      <div className="w-full overflow-clip absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
           alt="grid"
